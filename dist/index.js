2810e8cde3ac28c44974dc8c676b896f33ef761d
mo-auto <54212639+mo-auto@users.noreply.github.com>
2021-06-21 10:29:52 -0400

    fix: hello

--- dist/index.js
+++ dist/index.js
@@ -8885,7 +8885,7 @@ const commitfile = async( local_path, skip_ci, commit_message ) => {
 		message = commit_message;
 	}
 
-	return await toolkit.exec( `git commit -S "${message}"` );
+	return await toolkit.exec( `git commit -S -m "${message}"`, local_path );
 };
 
 const createPullRequestBranch = async( work_dir, current_branch ) => {
